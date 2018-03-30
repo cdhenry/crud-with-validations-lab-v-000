ee32f5728aec16f576b96f9e50249e59af30cc1f
Chris Henry <cdhenry@gmail.com>
2018-03-30 22:11:56 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,11 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  validates :title, uniqueness: true
   #validate :title_cannot_be_repeated_in_same_year
   validates :released, format: { with: /true|false/ }
   validates :release_year, presence: true, if: 'released == true'
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: 'release_year.present?'
   validates :artist_name, presence: true
-  validates :title, uniqueness: true if self.find_by(release_year: release_year)
 
   # def title_cannot_be_repeated_in_same_year
   #   title_test = Song.find_by(title: title)
